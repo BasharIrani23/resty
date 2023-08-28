32ddce7871bffef7853f5bf2dd9c257fa71190fe
Basharirani23 <super_boshbosh@hotmail.com>
2023-08-28 11:30:45 +0300

    Deployemnt purpose

--- resty/src/App.jsx
+++ resty/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
             });
             setResponseData(response.data);
         } catch (error) {
-            console.error("Error fetching data:", error);
+            console.error("Error fetching data :", error);
         }
     };
 
